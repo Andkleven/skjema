73b34bf580732ef1d2d01018d8e1601bf22a7dfd
And <andermat@nmbu.no>
2019-12-25 11:45:05 +0100

    f

--- app/models.py
+++ app/models.py
@@ -11,7 +11,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django import forms
 from decimal import Decimal
-from PIL import Image
 from rest_framework.authtoken.models import Token
 from django.db.models.signals import post_save
 from django.dispatch import receiver
